77c082d79c6fc4cde698e1ca1f4e5f7fa888ce0f
dillonmct <dmctaggart@academic.rrc.ca>
2021-11-21 00:15:40 -0600

    created seeds.rb to populate our db

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ApplicationRecord
   belongs_to :production_company
-  validates :title, :year, :duration, :description, :average_vote, presence: true
+  validates :title, :year, :duration, :description, :average_vite, presence: true
   validates :title, uniqueness: true
   validates :year, :duration, numericality: {only_integer:true}
-  validates :average_vote, numericality: true
+  validates :average_vite, numericality: true
 end
